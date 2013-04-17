b7668741148c5cf0c27ffc2faf021e73e9ca8610
Ana Krivokapic <akrivokapic1@gmail.com>
2013-04-17 21:20:50 +0200

    Add script for automating client installation

---
+++ ipa-client.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+USAGE="Usage: `basename $0` --server IPA_SERVER_HOSTNAME"
+
+if [ -n "$1" ] ; then
+    if [ "$1" = "--server" ] ; then
+	SERVER=$2
+    else
+	echo "Invalid option: $1"
+	echo $USAGE
+	exit 1
+    fi
+else
+    echo "Please provde IPA server hostname"
+    echo $USAGE
+    exit 1
+fi
+
+source ipa-config.sh
+
+DOMAIN=`echo $SERVER | cut -d '.' -f2-`
+IP=`ip addr show eth0 | grep "inet " | cut -d ' ' -f6 | cut -d '/' -f1`
+
+if [ `grep $IP /etc/hosts | wc -l` -eq 0 ] ; then
+    echo "Configuring /etc/hosts ..."
+    sudo IP=$IP sh -c 'echo "$IP    `hostname`" >> /etc/hosts'
+fi
+
+echo "Disabling updates-testing repo ..."
+sudo sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/fedora-updates-testing.repo
+
+echo "Updating system packages ..."
+sudo yum -y update
+
+echo "Installing custom built IPA rpms ..."
+cd $GIT_DIR/dist/rpms
+sudo yum -y localinstall freeipa-client* freeipa-python* freeipa-admintools*
+
+echo "Setting SELinux to permissive mode ..."
+sudo setenforce 0
+
+echo "Installing IPA client ..."
+sudo ipa-client-install --server=$SERVER --domain=$DOMAIN --enable-dns-updates -p admin -w $PASSWORD -U
