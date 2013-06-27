4db41958f2edc349af133bce9307c635cc181efc
Ana Krivokapic <akrivokapic1@gmail.com>
2013-06-27 18:36:12 +0200

    Make testing configuration more robust

--- ipa-test.sh
+++ ipa-test.sh
@@ -3,19 +3,15 @@
 source config/config.sh
 source lib/env-setup.sh
 
-echo "Installing python-nose ..."
+echo "Installing python-nose and selenium ..."
 sudo yum install python-nose --enablerepo=updates-testing -y
+sudo pip install selenium
 
 echo "Configuring testing environment ..."
 mkdir -p ~/.ipa
-
-if [ ! -f ~/.ipa/default.conf ] ; then
-    ln -s /etc/ipa/default.conf ~/.ipa/
-fi
-
-if [ `grep wait_for_attr=True /etc/ipa/default.conf | wc -l` -eq 0 ] ; then
-    sudo sh -c 'echo "wait_for_attr=True" >> /etc/ipa/default.conf'
-fi
+rm -f ~/.ipa/default.conf
+ln -s /etc/ipa/default.conf ~/.ipa/
+sudo sh -c 'echo "wait_for_attr=True" >> /etc/ipa/default.conf'
 
 cd $GIT_DIR
 rm -f tests/test_xmlrpc/service.crt
