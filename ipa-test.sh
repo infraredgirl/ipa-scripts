c940c09cee5cfce5185cb369cd0ca8b203f717a0
Ana Krivokapic <akrivokapic1@gmail.com>
2013-06-28 17:49:51 +0200

    Correct the path to service.crt
    Since 'tests' folder has been renamed to 'ipatests',
    correct the path to 'service.crt'.

--- ipa-test.sh
+++ ipa-test.sh
@@ -12,12 +12,11 @@ mkdir -p ~/.ipa
 rm -f ~/.ipa/default.conf
 ln -s /etc/ipa/default.conf ~/.ipa/
 sudo sh -c 'echo "wait_for_attr=True" >> /etc/ipa/default.conf'
-
-cd $GIT_DIR
-rm -f tests/test_xmlrpc/service.crt
+rm -f $GIT_DIR/ipatests/test_xmlrpc/service.crt
 echo "$PASSWORD" | kinit admin
 
 echo "Running tests ... "
+cd $GIT_DIR
 make
 ./make-testcert
 ./make-test
