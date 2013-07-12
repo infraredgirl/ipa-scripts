942847f6b181f1becd18d84665d1fdcce6e0961c
Ana Krivokapic <akrivokapic1@gmail.com>
2013-07-12 15:22:05 +0200

    Improve client installation
    Don't use --server and --domain options in ipa-client install.
    Setup /etc/resolv.conf to enable server discovery.

--- ipa-client.sh
+++ ipa-client.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-USAGE="Usage: `basename $0` --server IPA_SERVER_HOSTNAME"
+USAGE="Usage: `basename $0` --server-ip IPA_SERVER_IP_ADDRESS"
 
 if [ -n "$1" ] ; then
-    if [ "$1" = "--server" ] ; then
-	SERVER=$2
+    if [ "$1" = "--server-ip" ] ; then
+	SERVER_IP=$2
     else
 	echo "Invalid option: $1"
 	echo $USAGE
@@ -19,11 +19,12 @@ fi
 source config/config.sh
 source lib/env-setup.sh
 
-DOMAIN=`echo $SERVER | cut -d '.' -f2-`
-
 echo "Installing custom built IPA rpms ..."
 cd $GIT_DIR/dist/rpms
 sudo yum -y localinstall freeipa-client* freeipa-python* freeipa-admintools*
 
+echo "Setting up /etc/resolv.conf ..."
+sudo echo "nameserver $SERVER_IP" | cat - /etc/resolv.conf > /tmp/out && sudo mv -f /tmp/out /etc/resolv.conf
+
 echo "Installing IPA client ..."
-sudo ipa-client-install --server=$SERVER --domain=$DOMAIN --enable-dns-updates -p admin -w $PASSWORD -U
+sudo ipa-client-install --enable-dns-updates -p admin -w $PASSWORD -U
